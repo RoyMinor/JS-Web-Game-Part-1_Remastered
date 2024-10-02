47f471361d0a4c83f72a133cdf096415673503a8
Mandell1981 <mandell316@gmail.com>
2024-10-02 13:05:45 -0400

    added_inventory-box

--- index.js
+++ index.js
@@ -13,6 +13,39 @@ function newImage(imageSrc, left, bottom) {
     document.querySelector('.game-container').append(img); // Append the img element to the body
 }
 
+
+function newItem(imageSrc, left, bottom) {
+    const img = createImageElement(imageSrc, left, bottom); // Use the helper function
+    document.querySelector('.game-container').append(img); // Append the img element to the body
+
+    // Add event listener for click event
+    img.addEventListener('click', function() {
+        img.remove(); // Remove the img when it's clicked
+        let inventoryItem = document.createElement('img');
+        inventoryItem.src = imageSrc;
+        inventory.append(inventoryItem); // Append the inventory item to the inventory div
+    });
+}
+
+let inventory = document.createElement('div')
+
+function newInventory(){
+    inventory.style.position = 'fixed'
+    inventory.style.bottom = '0px';
+    inventory.style.left = '0px'
+    inventory.style.width = '100%'
+    inventory.style.height = '100px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+    inventory.style.border = '2px solid black'
+    inventory.style.backgroundColor = 'brown'
+    document.body.append(inventory)
+}
+
+newInventory()
+
 // Creating the characters
 newImage('assets/green-character.gif', 100, 150);
 newImage('assets/pine-tree.png', 200, 100);
@@ -20,16 +53,6 @@ newImage('assets/tree.png', 200, 300);
 newImage('assets/pillar.png', 350, 100);
 newImage('assets/crate.png', 150, 200);
 newImage('assets/well.png', 500, 425);
-
-function newItem(imageSrc, left, bottom) {
-    const img = createImageElement(imageSrc, left, bottom); // Use the helper function
-    document.querySelector('.game-container').append(img); // Append the img element to the body
-
-    // Add event listener for click event
-    img.addEventListener('dblclick', function() {
-        img.remove(); // Remove the img when it's clicked
-    });
-}
 
 // Creating the items
 newItem('assets/sword.png', 500, 405);
